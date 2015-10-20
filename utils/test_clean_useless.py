a690f60253714e4103901328d25cf57c31364e3a
Ilya Krylov <krylov@corp.sputnik.ru>
2015-10-20 17:39:20 +0300

    TESTING-2226: обновленные тесты для скрипта очистки ненужных сущностей salts_testresult.

--- utils/test_clean_useless.py
+++ utils/test_clean_useless.py
@@ -7,14 +7,65 @@ from clean_useless import Logger
 import os
 import shutil
 import random
+import psycopg2
 
-test_root_path = "test_data/useless"
-if not os.path.exists(test_root_path):
-    os.makedirs("test_data")
-    os.makedirs("test_data/useless")
 logger = Logger()
 
 class TestUselessTR(unittest.TestCase):
+
+    def _record_exists(self, id):
+        cursor = self._tr._conn.cursor()
+        query = "select * from salts_testresult where id = %s" % id
+        cursor.execute(query)
+        return bool(cursor.fetchall())
+
+    def _insert_dead_references(self, ref_values):
+        ref_values["test_id"] = "2015-10-16_17-18-12.8KdyK9"
+        ref_values["group"] = "Afisha.Deti"
+        ref_values["test_name"] = "Test.Name"
+        ref_values["target"] = "f2-afisha-101.ix.km:8080"
+        ref_values["version"] = "2.0.70.6afisha"
+        ref_values["rps"] = "r:line(1, 10, 1m) const(10, 60m)"
+        ref_values["graph_url"] = "http://grafana-test.ix.km/#/dashboard/db/tankresultstpl?var-system=fe_afisha&var-collector=f2-afisha-101.ix.km&from=20151016T131705&to=20151016T141812"
+        ref_values["generator"] = "tank-1"
+        ref_values["user"] = "suhov"
+        ref_values["ticket_id"] = "TESTING-0"
+        ref_values["mnt_url"] = "#LTM_URL#"
+        ref_values["comment"] = "No comments."
+        ref_values["test_status"] = "unk"
+        query = """ insert into salts_testresult 
+                    (test_id, dt_start, dt_finish, \"group\",
+                    test_name, target, version, rps, q99, q90, q50,
+                    http_errors_perc, net_errors_perc, graph_url, generator,
+                    \"user\", ticket_id, mnt_url, comments, test_status, meta,
+                    metrics, jm_log, modified_jmx, ph_conf, yt_conf, yt_log, 
+                    console_log, report_txt, jm_jtl, phout, jm_log_2)
+                    values ('%s', now(), now() + interval '1 hour', '%s', 
+                            '%s', '%s', '%s', '%s', 1677, 1117, 562, 
+                            0, 0, '%s', '%s', '%s', '%s', 
+                            '%s', '%s', '%s', NULL, 
+                            '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
+                    returning id
+                    """ % ( ref_values["test_id"], ref_values["group"], ref_values["test_name"],
+                            ref_values["target"], ref_values["version"], ref_values["rps"], 
+                            ref_values["graph_url"], ref_values["generator"],
+                            ref_values["user"], ref_values["ticket_id"], ref_values["mnt_url"], 
+                            ref_values["comment"], ref_values["test_status"],
+                            ref_values["metrics"], ref_values["jm_log"],
+                            ref_values["modified_jmx"], ref_values["ph_conf"],
+                            ref_values["yt_conf"], ref_values["yt_log"], 
+                            ref_values["console_log"], ref_values["report_txt"], 
+                            ref_values["jm_jtl"], ref_values["phout"], ref_values["jm_log_2"])
+        cursor = self._tr._conn.cursor()
+        cursor.execute(query)
+        self._tr._conn.commit()
+        ins_id = cursor.fetchone()[0]
+        return ins_id
+
+    def _delete_dead_references(self, del_ix):
+        query = "delete from salts_testresult where id = %d" % del_ix
+        self._tr._conn.cursor().execute(query)
+        self._tr._conn.commit()
 
     def _create_test_folder(self, root_path, folder_name):
         i = 0
@@ -33,16 +84,28 @@ class TestUselessTR(unittest.TestCase):
         fd.close()
         return full_path
         
-
     def _remove_test_folder(self, test_folder):
         if os.path.exists(test_folder):
             shutil.rmtree(test_folder)
 
-    def _create_test_options(self): 
-        self._test_result_folder_path = self._create_test_folder(test_root_path, "test-result") 
-        self._subfolder_path = self._create_test_folder(self._test_result_folder_path, "results")
-        self._subfolder = self._subfolder_path.split("/")[-1]
+    def _set_root_path(self, root_path):        
+        self._test_root_path = root_path
+
+    def _create_test_options(self, temp_folder_path = ""): 
+        if temp_folder_path:
+            if not os.path.exists(temp_folder_path):
+                os.makedirs(temp_folder_path)
+            self._test_result_folder_path = self._create_test_folder(
+                self._test_root_path, temp_folder_path) 
+            self._subfolder_path = self._create_test_folder(
+                self._test_result_folder_path, "results")
+            self._subfolder = self._subfolder_path.split("/")[-1]
+        else:
+            self._test_result_folder_path = self._test_root_path
+            self._subfolder = "results"
+            self._subfolder_path = "%s/%s" % (self._test_result_folder_path, self._subfolder)
         parser = OptionParser()
+        parser.add_option("--db-config", default = "test_db_salts.cfg")
         parser.add_option("--tr-path", default = self._test_result_folder_path)
         parser.add_option("--tr-subfolder", default = self._subfolder)
         parser.add_option("--log-filename", default = "test.log")
@@ -63,58 +126,90 @@ class TestUselessTR(unittest.TestCase):
             
 
     def setUp(self):
-        self._options = self._create_test_options()
+        self._temp_result_folder_path = "test_data"        
+ 
+    def test_remove_empty_folders(self):
+        self._set_root_path(self._temp_result_folder_path)
+        self._options = self._create_test_options(self._temp_result_folder_path)
+        self._tr = UselessTR(self._options, logger)
 
-        self._test_result_empty_folder_path = self._create_test_folder(test_root_path, "empty-test-result") 
-        self._empty_test_folder_path = self._create_test_folder(self._test_result_empty_folder_path, "empty-test-folder")
-        self._non_empty_test_folder_path = self._create_test_folder(self._test_result_empty_folder_path, "non-empty-test-folder")
-        self._create_test_file(self._non_empty_test_folder_path, "test")
+        test_result_empty_folder_path = self._create_test_folder(
+            self._test_root_path, "empty-test-result") 
+        empty_test_folder_path = self._create_test_folder(
+            test_result_empty_folder_path, "empty-test-folder")
+        non_empty_test_folder_path = self._create_test_folder(
+            test_result_empty_folder_path, "non-empty-test-folder")
+        self._create_test_file(non_empty_test_folder_path, "test")
 
-        self._db_records = [
+        self._tr.remove_empty_folders(test_result_empty_folder_path)
+        self.assertEqual(os.path.exists(empty_test_folder_path), False, 
+            "Empty %s folder exists (expected: it shouldn't be existed)." % 
+                empty_test_folder_path)
+        self.assertEqual(os.path.exists(non_empty_test_folder_path), True, 
+            "Non empty %s folder doesn't exist (expected: it should be existed)." % 
+                non_empty_test_folder_path)          
+
+        self._remove_test_folder(test_result_empty_folder_path)
+        self._remove_test_folder(self._temp_result_folder_path)
+
+    def test_useless_files(self):
+        self._set_root_path(self._temp_result_folder_path)
+        self._options = self._create_test_options(self._temp_result_folder_path)
+        self._tr = UselessTR(self._options, logger)
+
+        db_records = [
             ("1.txt", "3.abc", "dl1"), 
             ("", "dl2", "5.txt"),
             ("dl3", "", "")]
-        self._db_records = [tuple(self._add_path_prefix(self._subfolder, rec)) for rec in self._db_records]
-        self._db_expected = ["1.txt", "3.abc", "dl1", "dl2", "5.txt", "dl3"]
-        self._db_expected = self._add_path_prefix(self._subfolder, self._db_expected)
-        self._expected_ul_files = ["ul1.txt", "ul2.abc", "ul3.def"]
-        existed_log_filenames = ["1.txt", "3.abc", "5.txt"] + self._expected_ul_files
+        db_records = [tuple(self._add_path_prefix(self._subfolder, rec)) 
+                        for rec in db_records]
+        db_expected = ["1.txt", "3.abc", "dl1", "dl2", "5.txt", "dl3"]
+        db_expected = self._add_path_prefix(self._subfolder, db_expected)
+        expected_ul_files = ["ul1.txt", "ul2.abc", "ul3.def"]
+        existed_log_filenames = ["1.txt", "3.abc", "5.txt"] + expected_ul_files
         random.shuffle(existed_log_filenames)
         for elf in existed_log_filenames:
             self._create_test_file(self._subfolder_path, elf)
-        self._existed_log_files = self._add_path_prefix(self._subfolder, existed_log_filenames) 
-        self._expected_ul_files = self._add_path_prefix(self._subfolder_path, self._expected_ul_files)
-        self._expected_dead_ref = ["dl1", "dl2", "dl3"]
-        self._expected_dead_ref = self._add_path_prefix(self._subfolder, self._expected_dead_ref)
-        self._tr = UselessTR(self._options, logger)
-        self._db_ref = None
- 
-    def test_remove_empty_folders(self):
-        self._tr.remove_empty_folders(self._test_result_empty_folder_path)
-        self.assertEqual(os.path.exists(self._empty_test_folder_path), False, 
-            "Empty %s folder exists (expected: it shouldn't be existed)." % self._empty_test_folder_path)
-        self.assertEqual(os.path.exists(self._non_empty_test_folder_path), True, 
-            "Non empty %s folder doesn't exist (expected: it should be existed)." % self._non_empty_test_folder_path)           
-    def test_useless_files(self):
-        self._set_db_ref()
-        ul_files = self._tr.useless_files(self._existed_log_files, self._db_ref)
-        self.assertEqual(ul_files.sort(), self._expected_ul_files.sort(), 
-            "Useless files list isn't expected. Result: %s. Expected: %s" % (ul_files, self._expected_ul_files))
+        existed_log_files = self._add_path_prefix(self._subfolder, existed_log_filenames) 
+        expected_ul_files = self._add_path_prefix(self._subfolder_path, expected_ul_files)
+
+        db_ref = self._tr._get_db_ref(db_records)
+        self.assertEqual(db_ref, db_expected, 
+            "Useless files isn't expected. Result: %s. Expected: %s." % (db_ref, db_expected))
+
+        ul_files = self._tr.useless_files(existed_log_files, db_ref)
+        self.assertEqual(ul_files.sort(), expected_ul_files.sort(), 
+            "Useless files list isn't expected. Result: %s. Expected: %s" % (
+                ul_files, expected_ul_files))
+
         self._tr.remove_useless_files(ul_files)
         for ulf in ul_files:
             self.assertEqual(os.path.exists(ulf), False, 
                 "%s useless file exists (expected: it shouldn't be existed)." % ulf)
+        self._remove_test_folder(self._temp_result_folder_path)
 
-    def test_dead_db_file_references(self):
-        self._set_db_ref()
-        dead_ref = self._tr.dead_db_file_references(self._existed_log_files, self._db_ref)
+    def test_clear_db_file_references(self): 
+        self._set_root_path("test_results")
+        self._options = self._create_test_options()
+        self._tr = UselessTR(self._options, logger)
+
+        ref_values = {}
+        for lff in self._tr._log_files_fields:
+            ref_values[lff] = "dl_%s" % lff
+        self._expected_dead_ref = self._add_path_prefix(self._subfolder, ref_values)
+        cur_ix = self._insert_dead_references(ref_values) 
+        db_ref = self._tr.db_file_references()
+        existed_log_files = self._tr.saved_log_files()
+        dead_ref = self._tr.dead_db_file_references(existed_log_files, db_ref)
         self.assertEqual(dead_ref.sort(), self._expected_dead_ref.sort(),
             "Dead references list isn't expected. Result: %s. Expected: %s." % (dead_ref, self._expected_dead_ref))
         self._tr.log_dead_references(dead_ref)
+        self._tr.clear_dead_references(dead_ref)
+        self._delete_dead_references(cur_ix)
+        self.assertEqual(self._record_exists(cur_ix), False, "New record wasn't removed.")
 
     def tearDown(self):
-        self._remove_test_folder(self._test_result_folder_path)
-        self._remove_test_folder(self._test_result_empty_folder_path)
+        pass
 
 if __name__ == "__main__":
     unittest.main()
