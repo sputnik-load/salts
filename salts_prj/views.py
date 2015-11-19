428209fc5785c44b4a8d5725813f9a8b4ac260a6
Ilya Krylov <krylov@corp.sputnik.ru>
2015-11-19 16:11:43 +0300

    Коммит перед переключением на master.

--- salts_prj/views.py
+++ salts_prj/views.py
@@ -2,13 +2,20 @@
 
 import os
 import simplejson
+import logging
+import time
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
+from salts_prj.tankapi_client import tankapi_client
 
 
-LT_PATH = "/home/krylov/prj/loadtest"
+LT_PATH = "/home/krylov/prj/test-repo"
+SERVER_HOST_DEFAULT = "localhost"
+SERVER_PORT_DEFAULT = "8888"
 
+
+logger = logging.getLogger("salts")
 
 def ini_files():
     ini = []
@@ -21,12 +28,29 @@ def ini_files():
     return ini
 
 
+def run_test_api(ini_path):
+    client1 = tankapi_client.tankapi_client(SERVER_HOST_DEFAULT,
+                                            SERVER_PORT_DEFAULT,
+                                            logger)
+    logger.info("Preparing ...")
+    shoot1 = client1.run_new(config_contents=ini_path,
+                             stage="start")
+    logger.info(shoot1)
+    while not client1.destination_reached(shoot1["session"], "prepare"):
+        logger.info("Test not prepared yet.")
+        time.sleep(5)
+    logger.info("Prepared OK")
+    logger.info("Shooting ...")
+    client1.run_given(shoot1["session"])
+    while not client1.destination_reached(shoot1["session"], "finished"):
+        logger.info("Test not finished yet.")
+        time.sleep(5)
+
 def tests_list(request):
     if request.POST.has_key("client_response"):
-        x = request.POST["client_response"]
+        x = request.POST["ini_path"]
         response_dict = {}
         response_dict.update({"server_response": x})
-        # response_dict.update({"csrftoken": request.POST["csrfmiddlewaretoken"]})
         return HttpResponse(simplejson.dumps(response_dict),
                             mimetype="application/javascript")
     context = {}
@@ -34,7 +58,7 @@ def tests_list(request):
     i = 1
     for name in ini_files():
         context["configs"].append({"name": name,
-                                   "id": "ini_%d" % i})
+                                   "id": "%d" % i})
         i += 1
     context.update(csrf(request))
     return render_to_response("tests_list.html", context)
