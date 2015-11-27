06669e034f5665f9f2fea19d74cb5064d53759be
Alexander Sukhov <suhov@corp.sputnik.ru>
2015-11-27 13:38:43 +0300

    добавил target в список и в фильтр

--- salts/admin.py
+++ salts/admin.py
@@ -5,8 +5,8 @@ from salts.models import *
 # Register your models here.
 class TestResultAdmin(admin.ModelAdmin):
     #list_display = ('id', 'status', 'test_status', 'dt_start', 'dt_finish', 'results')
-    list_display = ('test_name', 'group', 'version', 'test_status', 'rps', 'show_test_len', 'q99', 'q90', 'q50', 'http_errors_perc', 'net_errors_perc', 'generator', 'show_graph_url', 'user', 'ticket_id', 'test_id', )
-    list_filter = ['group', 'test_status', 'generator', 'user', ]
+    list_display = ('test_name', 'group', 'version', 'test_status', 'rps', 'show_test_len', 'q99', 'q90', 'q50', 'http_errors_perc', 'net_errors_perc', 'generator', 'target', 'show_graph_url', 'user', 'ticket_id', 'test_id', )
+    list_filter = ['group', 'test_status', 'generator', 'user', 'target']
     readonly_fields = ('meta',)
 
     def show_graph_url(self, instance):
