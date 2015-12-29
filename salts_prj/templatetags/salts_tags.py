99617be713cd09a76f9cf37ec4f91654722b7971
Ilya Krylov <krylov@corp.sputnik.ru>
2015-12-29 17:56:21 +0300

    TESTING-2549 добавлены вывод и вычисление длительности теста.

--- salts_prj/templatetags/salts_tags.py
+++ salts_prj/templatetags/salts_tags.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import datetime
 from django import template
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -12,3 +13,7 @@ def active(request, url):
     if request.path.startswith(reverse(url)):
         return 'active'
     return ''
+
+@register.simple_tag
+def duration(dt_start, dt_finish):
+    return dt_finish - dt_start
