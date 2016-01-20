31502d1beefbeadee4bdfb856285f38f38a7f59d
Alexander Sukhov <suhov@corp.sputnik.ru>
2016-01-20 13:35:25 +0300

    добавил scenario_id для редактирования в списке

--- salts/admin.py
+++ salts/admin.py
@@ -5,10 +5,10 @@ from salts.models import *
 # Register your models here.
 class TestResultAdmin(admin.ModelAdmin):
     #list_display = ('id', 'status', 'test_status', 'dt_start', 'dt_finish', 'results')
-    list_display = ('id', 'test_name', 'group', 'version', 'test_status', 'rps', 'show_test_len', 'q99', 'q90', 'q50', 'http_errors_perc', 'net_errors_perc', 'generator', 'target', 'show_graph_url', 'user', 'ticket_id', 'test_id', )
+    list_display = ('id', 'test_name', 'group', 'version', 'test_status', 'rps', 'show_test_len', 'q99', 'q90', 'q50', 'http_errors_perc', 'net_errors_perc', 'generator', 'target', 'show_graph_url', 'user', 'ticket_id', 'test_id', 'scenario_id', )
     list_display_links = ('id', 'test_id', )
-    list_filter = ['group', 'test_status', 'generator', 'user', 'target']
-    list_editable = ('test_name', 'test_status', 'rps', 'ticket_id', )
+    list_filter = ['group', 'test_status', 'generator', 'user', 'target', 'scenario_id']
+    list_editable = ('test_name', 'test_status', 'rps', 'ticket_id', 'scenario_id', )
     list_per_page = 15
     readonly_fields = ('meta',)
 
