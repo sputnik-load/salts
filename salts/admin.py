d00ea4157850bef337ffcc053fb648e5da027fcb
Ilya Krylov <krylov@corp.sputnik.ru>
2016-02-01 19:10:14 +0300

    TESTING-2667 поле редактирования комментария заменено на многострочное

--- salts/admin.py
+++ salts/admin.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django import forms
 from salts.models import *
+
+
+class TestResultAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TestResultAdminForm, self).__init__(*args, **kwargs)
+        self.fields['comments'].widget = admin.widgets.AdminTextareaWidget()
+
 
 # Register your models here.
 class TestResultAdmin(admin.ModelAdmin):
+    form = TestResultAdminForm
     #list_display = ('id', 'status', 'test_status', 'dt_start', 'dt_finish', 'results')
-    list_display = ('id', 'test_name', 'group', 'version', 'test_status', 'rps', 'show_test_len', 'q99', 'q90', 'q50', 'http_errors_perc', 'net_errors_perc', 'generator', 'target', 'show_graph_url', 'user', 'ticket_id', 'test_id', 'scenario_id', )
+    list_display = ('id', 'test_name', 'group', 'version', 'test_status', 'comments',
+                    'rps', 'show_test_len', 'q99', 'q90', 'q50',
+                    'http_errors_perc', 'net_errors_perc', 'generator', 'target',
+                    'show_graph_url', 'user', 'ticket_id', 'test_id', 'scenario_id',)
     list_display_links = ('id', 'test_id', )
     list_filter = ('group', 'test_status', 'generator', 'user', )
-    list_editable = ('test_name', 'test_status', 'rps', 'ticket_id', 'scenario_id', )
+    list_editable = ('test_name', 'test_status', 'comments', 'rps', 'ticket_id',
+                     'scenario_id',)
     list_per_page = 15
     readonly_fields = ('meta',)
     search_fields = ('test_name', 'group', 'target', 'scenario_id', 'ticket_id', 'test_id', )
@@ -18,6 +31,7 @@ class TestResultAdmin(admin.ModelAdmin):
     show_graph_url.allow_tags = True
     def show_test_len(self, instance):
         return str('%s' % (instance.dt_finish - instance.dt_start))
+
 
 
 admin.site.register(TestResult, TestResultAdmin)
