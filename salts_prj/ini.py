df9cab11466c8f792db55f470a15f62be9f913af
Ilya Krylov <krylov@corp.sputnik.ru>
2016-04-04 11:38:33 +0300

    Из поиска ini файлов теперь можно исключить файлы из заданного списка.

--- salts_prj/ini.py
+++ salts_prj/ini.py
@@ -1,13 +1,24 @@
 import os
+from glob import glob
 from settings import LT_PATH, EXCLUDE_INI_FILES
 
 
 def ini_files(dir_path, exclude_names):
     ini = []
+    specific_names = {}
+    specific_names[dir_path] = []
+    for spec_name in exclude_names:
+        specific_names[dir_path] += glob(os.path.join(dir_path, spec_name))
+    for root, dirs, files in os.walk(dir_path, topdown=False):
+        for name in dirs:
+            full = os.path.join(root, name)
+            specific_names[full] = []
+            for spec_name in exclude_names:
+                specific_names[full] += glob(os.path.join(full, spec_name))
     for root, dirs, files in os.walk(dir_path, topdown=False):
         for name in files:
             full_path = os.path.join(root, name)
             file_name, file_ext = os.path.splitext(full_path)
-            if file_ext == ".ini":
+            if file_ext == ".ini" and (not full_path in specific_names[root]):
                 ini.append(full_path)
     return ini
