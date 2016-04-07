8187aef33514da19e9f83bd5911464c88d9dac52
Ilya Krylov <krylov@corp.sputnik.ru>
2016-04-07 11:48:16 +0300

    Добавил класс для управления ini-файлами и соответствующими им записями в таблице TestIni

--- salts_prj/ini.py
+++ salts_prj/ini.py
@@ -1,6 +1,7 @@
 import os
 from glob import glob
 from settings import LT_PATH, EXCLUDE_INI_FILES
+from salts.models import TestIni
 
 
 def ini_files(dir_path, exclude_names):
@@ -22,3 +23,35 @@ def ini_files(dir_path, exclude_names):
             if file_ext == ".ini" and (not full_path in specific_names[root]):
                 ini.append(full_path)
     return ini
+
+
+class IniCtrl(object):
+
+    def __init__(self):
+        self.filepathes = []
+        self.db_ini_files = []
+        self.dir_path = ""
+
+    def find_ini_files(self, dir_path, exclude_names):
+        self.filepathes = ini_files(dir_path, exclude_names)
+        self.dir_path = dir_path
+        return self.filepathes
+
+    def get_root(self):
+        return self.dir_path
+
+    def get_ini_files(self):
+        return self.filepathes
+
+    def select_db_ini_files(self):
+        self.db_ini_files = TestIni.objects.all()
+
+    def get_db_ini_files(self):
+        if not self.db_ini_files:
+            self.select_db_ini_files()
+        return self.db_ini_files
+
+    def sync(self):
+        pass
+
+
