631b4b456747ae10ca1b6bb76a682d287c490b21
Ilya Krylov <krylov@corp.sputnik.ru>
2016-05-27 18:29:10 +0300

    TESTING-2958 fix: не снималась блокировка танка при прерывании по Ctrl+C теста, запущенного с помощью run.sh

--- salts/tankmanager.py
+++ salts/tankmanager.py
@@ -5,6 +5,7 @@ import time
 import os
 import shutil
 import json
+import re
 from salts.logger import Logger
 from salts.api_client import TankClient
 from salts_prj.settings import LT_PATH
@@ -149,17 +150,20 @@ class TankManager(object):
 
     def interrupt(self, shooting):
         resp = None
-        try:
-            client = TankClient(shooting.tank.host, shooting.tank.port)
-            resp = client.status(shooting.test_id)
-            client.stop(shooting.test_id)
-        except Exception, exc:
-            log.info("Exception when test "
-                     "has been interrupted: %s" % exc)
+        if re.match('\d+_0+', shooting.test_id):
+            try:
+                client = TankClient(shooting.tank.host, shooting.tank.port)
+                resp = client.status(shooting.test_id)
+                client.stop(shooting.test_id)
+            except Exception, exc:
+                log.info("Exception when test "
+                        "has been interrupted: %s" % exc)
+        log.info("TankManager.interrupt. Shooting.test_id: %s" % shooting.test_id)
         try:
             log.info("The test id=%s is stopped." % shooting.test_id)
             self.free(shooting.tank.id)
-            if (resp and resp.get('current_stage') == 'poll') or not resp:
+            if (resp and resp.get('current_stage') == 'poll') \
+               or (not resp and shooting.status != 'P'):
                 thread_data = {'shooting': shooting}
                 t = threading.Thread(name="Change Test Status",
                                      target=self._change_test_status,
