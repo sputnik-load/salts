f922baeaf989175bee00f60232a7e146073eccc8
Ilya Krylov <krylov@corp.sputnik.ru>
2016-07-20 13:51:23 +0300

    Заменил в именах функций и переменных test_id на scenario_id

--- salts_prj/ini.py
+++ salts_prj/ini.py
@@ -68,7 +68,7 @@ class IniCtrl(object):
             log.info("Group '%s' has been added." % IniCtrl.DEFAULT_GROUP)
         self.default_group_id = g.id
 
-    def _test_id_from_ini(self, scenario_path):
+    def _scenario_id_from_ini(self, scenario_path):
         config = ConfigParser()
         config.read(os.path.join(self.dir_path, scenario_path))
         try:
@@ -77,9 +77,9 @@ class IniCtrl(object):
         except (NoOptionError, NoSectionError):
             return 0
 
-    def _add_test_id(self, scenario_path, test_id):
-        old_test_id = self._test_id_from_ini(scenario_path)
-        if test_id == old_test_id:
+    def _add_scenario_id(self, scenario_path, scenario_id):
+        old_scenario_id = self._scenario_id_from_ini(scenario_path)
+        if scenario_id == old_scenario_id:
             return
 
         section_line = "[%s]" % IniCtrl.SALTS_SECTION
@@ -105,20 +105,20 @@ class IniCtrl(object):
                 if re.match(pat, strip_line):
                     del(content[ix])
                     content.insert(ix, "%s = %s\n" \
-                                   % (IniCtrl.SCENARIO_ID_OPTION, test_id))
+                                   % (IniCtrl.SCENARIO_ID_OPTION, scenario_id))
                     break
             if strip_line == section_line:
                 section_found = True
-                if not old_test_id:
+                if not old_scenario_id:
                     content.insert(ix + 1,
                                    "%s = %s\n" \
-                                   % (IniCtrl.SCENARIO_ID_OPTION, test_id))
+                                   % (IniCtrl.SCENARIO_ID_OPTION, scenario_id))
                     break
             ix += 1
         with open(ini_path, "w") as f:
             f.writelines("".join(content))
 
-    def _last_test_id(self):
+    def _last_scenario_id(self):
         cursor = connection.cursor()
         cursor.execute("SELECT id FROM salts_scenario "
                        "ORDER BY id DESC LIMIT 1")
@@ -147,7 +147,7 @@ class IniCtrl(object):
         res = Scenario.objects.filter(status=status_value)
         return [r.scenario_path for r in res]
 
-    def get_test_id(self, scenario_path, from_db=False):
+    def get_scenario_id(self, scenario_path, from_db=False):
         if from_db:
             try:
                 t = Scenario.objects.get(scenario_path=scenario_path)
@@ -157,7 +157,7 @@ class IniCtrl(object):
         else:
             if not self.dir_path:
                 return 0
-            return self._test_id_from_ini(scenario_path)
+            return self._scenario_id_from_ini(scenario_path)
 
     def get_group_id(self, scenario_path):
         try:
@@ -186,16 +186,16 @@ class IniCtrl(object):
         scenario_pathes = ini_files(self.dir_path)
         absent_ini_pathes = []
         for spath in scenario_pathes:
-            ini_test_id = self.get_test_id(spath, from_db=False)
-            db_test_id = self.get_test_id(spath, from_db=True)
-            if ini_test_id:
-                if db_test_id:
-                    if ini_test_id == db_test_id:
+            ini_scenario_id = self.get_scenario_id(spath, from_db=False)
+            db_scenario_id = self.get_scenario_id(spath, from_db=True)
+            if ini_scenario_id:
+                if db_scenario_id:
+                    if ini_scenario_id == db_scenario_id:
                         continue
                     else:
-                        self._add_test_id(spath, db_test_id)
+                        self._add_scenario_id(spath, db_scenario_id)
                 else:
-                    res = Scenario.objects.filter(id=ini_test_id)
+                    res = Scenario.objects.filter(id=ini_scenario_id)
                     if res:
                         if os.path.exists(os.path.join(self.dir_path,
                                                        res[0].scenario_path)):
@@ -205,19 +205,19 @@ class IniCtrl(object):
                                                        res[0].scenario_path))
                         else:
                             res[0].delete()
-                    t = Scenario(id=ini_test_id,
+                    t = Scenario(id=ini_scenario_id,
                                 scenario_path=spath,
                                 group_id=self.default_group_id,
                                 status='A')
                     t.save()
             else:
-                if db_test_id:
-                    self._add_test_id(spath, db_test_id)
+                if db_scenario_id:
+                    self._add_scenario_id(spath, db_scenario_id)
                 else:
                     absent_ini_pathes.append(spath)
         if absent_ini_pathes:
             with open("/tmp/1.csv", "w") as f:
-                id = self._last_test_id() + 1
+                id = self._last_scenario_id() + 1
                 for spath in absent_ini_pathes:
                     rec = []
                     rec.append(str(id))
@@ -225,7 +225,7 @@ class IniCtrl(object):
                     rec.append("A")
                     rec.append(str(self.default_group_id))
                     f.write(";".join(rec) + "\n")
-                    self._add_test_id(spath, id)
+                    self._add_scenario_id(spath, id)
                     id += 1
             with open("/tmp/1.csv", "r") as f:
                 cursor = connection.cursor()
