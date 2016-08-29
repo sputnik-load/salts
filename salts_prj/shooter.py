13a3a8c1f2585eb8f96881bf35f3b99f2adb636a
Ilya Krylov <krylov@corp.sputnik.ru>
2016-08-29 11:25:37 +0300

    TESTING-3100 добавил возможность остановки теста по shooting_id

--- salts_prj/shooter.py
+++ salts_prj/shooter.py
@@ -18,7 +18,6 @@ from requesthelper import request_get_value
 
 @postpone
 def start_shooting_process(**kwargs):
-    log.info("testing3100: start shooting process.")
     tank_manager.shoot(**kwargs)
 
 
@@ -29,7 +28,17 @@ class ShooterView(View):
         return super(ShooterView, self).dispatch(*args, **kwargs)
 
     def get(self, request):
+        shooting_id = request_get_value(request, 'stop')
+        if shooting_id:
+            return self.stop_shooting(shooting_id)
+
+        scenario_id = request_get_value(request, 'scid')
+        tank_host = request_get_value(request, 'tank_host')
         custom_data = request_get_value(request, 'custom_data')
+        return self.start_shooting(scenario_id, tank_host, custom_data,
+                                   request.user)
+
+    def start_shooting(self, scenario_id, tank_host, custom_data, user):
         json_str = '{}'
         if custom_data:
             b64line = unquote_plus(custom_data)
@@ -38,13 +47,11 @@ class ShooterView(View):
         if 'salts' not in config:
             config['salts'] = {}
         if 'api_user' not in config['salts']:
-            config['salts']['api_user'] = request.user.username
+            config['salts']['api_user'] = user.username
         if 'api_key' not in config['salts']:
             user = User.objects.get(username=config['salts']['api_user'])
             tokens = Token.objects.filter(user_id=user.id)
             config['salts']['api_key'] = tokens[0].key
-        tank_host = request_get_value(request, 'tank_host')
-        scenario_id = request_get_value(request, 'scid')
         log.info("Scenario ID: %s" % scenario_id)
         scenario = Scenario.objects.get(id=scenario_id)
         tank = Tank.objects.get(host=tank_host)
@@ -58,8 +65,22 @@ class ShooterView(View):
             if shootings:
                 sh = shootings[0]
                 if sh.status == 'R':
-                    response_dict['start_time'] = sh.start
                     response_dict['id'] = sh.id
                     break
+        return HttpResponse(json.dumps(response_dict),
+                            content_type="application/json")
+
+    def stop_shooting(self, id):
+        shooting = Shooting.objects.get(id=id)
+        tank_manager.interrupt(shooting)
+        while True:
+            time.sleep(1)
+            shooting = Shooting.objects.get(id=id)
+            if shooting.finish:
+                shooting.status = 'I'
+                shooting.save()
+                break
+        response_dict = {}
+        response_dict['stopped'] = True
         return HttpResponse(json.dumps(response_dict),
                             content_type="application/json")
