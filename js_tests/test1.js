7c4bbec39b4d3ac000c023d10806ca55cbc58b3b
Ilya Krylov <krylov@corp.sputnik.ru>
2017-01-30 16:22:21 +0300

    Изменил тест ltconfigeditor в связи с переименованием customdata

--- js_tests/test1.js
+++ js_tests/test1.js
@@ -20,7 +20,7 @@ QUnit.test("Text Element Test From X-Editable", function(assert) {
 });
 
 
-QUnit.test("SALTS: Custom Element Test", function(assert) {
+QUnit.test("LT Config Editor Test", function(assert) {
 	var done = assert.async();
 	var testData = {
 		test_name: "LT Config",
@@ -32,14 +32,33 @@ QUnit.test("SALTS: Custom Element Test", function(assert) {
 		port: "7654",
 		s: "1"
 	};
+	var expected = {
+		test_name: testData["test_name"],
+		rps: testData["rps"],
+		rampup: testData["rampup"] / 1000,
+		testlen: testData["testlen"] / 1000,
+		rampdown: testData["rampdown"] / 1000,
+		target: testData["target"],
+		port: testData["port"],
+	};
 	var binTestData = jsonstr2bin(JSON.stringify(testData));
-	var htmlCode = "<a href='#' id='a' data-type='customdata' " +
+	var htmlCode = "<a href='#' id='a' data-type='ltconfigeditor' " +
 				   "data-value='" + binTestData + "' data-title='xyz'></a>";
 	var e = $(htmlCode).appendTo("#qunit-fixture").editable();
 	e.click();
 	var p = e.data("editableContainer").tip();
 	assert.ok(p.is(":visible"), "popover was visible");
-	assert.equal(p.find("input[name=test_name]").val(), "LT Config", "Test Name displays");
+	simpleTest = function(name, comment) {
+		assert.equal(p.find("input[name=" + name + "]").val(),
+					 expected[name], comment);	
+	};
+	simpleTest("test_name", "Test Name displays");
+	simpleTest("rps", "RPS displays");
+	simpleTest("rampup", "Ramp Up displays");
+	simpleTest("testlen", "Const Len displays");
+	simpleTest("rampdown", "Ramp Down displays");
+	simpleTest("target", "Target displays");
+	simpleTest("port", "Port displays");
 	p.find(".editable-cancel").click();
 	setTimeout(function() {
 		assert.ok(!p.is(":visible"), "popover was removed");
