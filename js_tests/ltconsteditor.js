24a97c1182e7ec6c644e51f643cf3702154cfefd
Ilya Krylov <krylov@corp.sputnik.ru>
2017-01-31 16:58:45 +0300

    Поправил исходные данные теста и тест title

--- js_tests/ltconsteditor.js
+++ js_tests/ltconsteditor.js
@@ -1,19 +1,20 @@
 QUnit.test("LT Const Editor Test", function(assert) {
 	var done = assert.async();
-	var line = "constant for 100 rps for 00:10:00";
+	var rps = 100;
+	var duration = 600; // seconds
 	var expected = {
+		"line": "constant for 100 rps for 00:10:00",
 		"title": "Constant Load For 100 rps",
 		"value": "600"
 	};
-	var htmlCode = "<a href='#' id='a' data-type='ltconsteditor' " +
-				   "data-value='" + line + "'></a>";
+	var htmlCode = htmlCodeLTConstEditor("abc", 100, 600);
 	var e = $(htmlCode).appendTo("#qunit-fixture").editable();
 	e.click();
 	var p = e.data("editableContainer").tip();
 	assert.ok(p.is(":visible"), "popover was visible");
 	assert.equal(p.find("input[name=testlen]").val(),
-				 expected["value"], "Const Duration Length displays.");	
-	assert.equal(p.find("input[name=testlen]").attr("title"),
+				 expected["value"], "Const Duration Test.");
+	assert.equal(p.find(".popover-title").text(),
 				 expected["title"], "Window Title Test.");	
 	p.find(".editable-cancel").click();
 	setTimeout(function() {
