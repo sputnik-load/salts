22b74ce56fc6e9ecbf21472286f41f530fae1e29
Ilya Krylov <krylov@corp.sputnik.ru>
2017-01-31 13:31:28 +0300

    Пополнил salts.js новыми общими функциями fromHHMMSS, bin2jsonstr, jsonstr2bin, ms2sec, sec2ms. Обновил функцию toHHMMSS.

--- salts/static/salts/js/salts.js
+++ salts/static/salts/js/salts.js
@@ -1,12 +1,62 @@
 function toHHMMSS(seconds) {
 	var pad = function(num) {
+		if (isNaN(num)) {
+			throw "Incorrect input";
+		}
 		if (num < 10) {
 			num = "0" + num;
 		}
 		return num;
 	}
-    var hh = Math.floor(seconds / 3600);
-    var mm = Math.floor((seconds - (hh * 3600)) / 60);
-    var ss = Math.floor(seconds - (hh * 3600) - (mm * 60));
-    return pad(hh) + ':' + pad(mm) + ':' + pad(ss);
+	try {
+		var hh = Math.floor(seconds / 3600);
+		var mm = Math.floor((seconds - (hh * 3600)) / 60);
+		var ss = Math.floor(seconds - (hh * 3600) - (mm * 60));
+    	return pad(hh) + ':' + pad(mm) + ':' + pad(ss);
+	} catch(err) {
+		return err;
+	}
+}
+
+
+function fromHHMMSS(line) {
+	var values = line.split(":");
+	if (values.length != 3) {
+		return "Incorrect input";
+	}
+	var str2int = function(str) {
+		if (str.length != 2)
+			throw "Incorrect input";
+		var v = parseInt(str, 10);
+		if (isNaN(v))
+			throw "Incorrect input";
+		return v;
+	}
+	try {
+		return 3600*str2int(values[0]) +
+			   60*str2int(values[1]) +
+			   str2int(values[2]);
+	} catch(err) {
+		return err;
+	}
+}
+
+
+function bin2jsonstr(binStr) {
+  return decodeURIComponent(atob(binStr));
+}
+
+
+function jsonstr2bin(jsonStr) {
+  return btoa(encodeURIComponent(jsonStr));
+}
+
+
+function ms2sec(ms) {
+	return ms / 1000;
+}
+
+
+function sec2ms(sec) {
+	return sec * 1000;
 }
