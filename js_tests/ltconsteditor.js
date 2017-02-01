35f4b4fcbffaef2401cddb66f2ed3f63320c54c2
Ilya Krylov <krylov@corp.sputnik.ru>
2017-02-01 14:54:59 +0300

    Добавил проверку на правильность сохранения нового значения постоянной нагрузки

--- js_tests/ltconsteditor.js
+++ js_tests/ltconsteditor.js
@@ -9,8 +9,7 @@ QUnit.test("LT Const Editor Cancel Test", function(assert) {
 	};
 	var htmlCode = $.htmlCodeLTConstEditor("abc", 100, 600);
 	var e = $(htmlCode).appendTo("#qunit-fixture").editable();
-	assert.equal(e.attr("data-value"),
-				 expected["view"], "Value View Test.");
+	assert.equal(e.data("editable").value, expected["view"], "Value View Test.");
 	e.click();
 	var p = e.data("editableContainer").tip();
 	assert.ok(p.is(":visible"), "popover was visible");
@@ -30,13 +29,19 @@ QUnit.test("LT Const Editor New Value Test", function(assert) {
 	var done = assert.async();
 	var rps = 100;
 	var duration = 600; // seconds
+	var newDuration = 1000; // seconds
+	var expectedView = "constant for 100 rps for 00:16:40";
 	var htmlCode = $.htmlCodeLTConstEditor("abc", 100, 600);
 	var e = $(htmlCode).appendTo("#qunit-fixture").editable();
 	e.click();
 	var p = e.data("editableContainer").tip();
+	assert.equal(p.find("input[name=testlen]").val(),
+				 "600", "Const Repeat Duration Test.");
+	p.find("input[name=testlen]").val(newDuration);
 	p.find("form").submit();
 	setTimeout(function() {
 		assert.ok(!p.is(":visible"), "popover was removed");
+		assert.equal(e.data("editable").value, expectedView, "New Duration saved.");
 		done();
 	}, 300);
 });
