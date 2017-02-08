771627ca90964df8ba4c707e70c831597c779a34
Ilya Krylov <krylov@corp.sputnik.ru>
2017-02-08 16:27:40 +0300

    Добавил в интерфейс кнопку добавления нового профиля нагрузки.

--- js_tests/ltconfigeditor.js
+++ js_tests/ltconfigeditor.js
@@ -1,27 +1,31 @@
-QUnit.test("LT Config Editor Test", function(assert) {
+var testData = {
+	test_name: "LT Config",
+	rps: 5,
+	rampup: 15000,
+	testlen: 60000,
+	load: {"const": 60000,
+		   "step" : [10, 2, 10000]},
+	rampdown: 10000,
+	target: "xyz",
+	port: 7654,
+	s: 1
+};
+
+var expected = {
+	test_name: testData.test_name,
+	rps: testData.rps,
+	rampup: testData.rampup / 1000,
+	testlen: testData.testlen / 1000,
+	rampdown: testData.rampdown / 1000,
+	target: testData.target,
+	port: testData.port,
+};
+
+QUnit.test("LT Config Editor: No User Load", function(assert) {
 	var done = assert.async();
-	var testData = {
-		test_name: "LT Config",
-		rps: "5",
-		rampup: "15000",
-		testlen: "60000",
-		rampdown: "10000",
-		target: "xyz",
-		port: "7654",
-		s: "1"
-	};
-	var expected = {
-		test_name: testData["test_name"],
-		rps: testData["rps"],
-		rampup: testData["rampup"] / 1000,
-		testlen: testData["testlen"] / 1000,
-		rampdown: testData["rampdown"] / 1000,
-		target: testData["target"],
-		port: testData["port"],
-	};
 	var binTestData = jsonstr2bin(JSON.stringify(testData));
-	var htmlCode = "<a href='#' id='a' data-type='ltconfigeditor' " +
-				   "data-value='" + binTestData + "' data-title='xyz'></a>";
+	var htmlCode = "<a href=# id=a data-type=ltconfigeditor " +
+				   "data-value='" + binTestData + "' data-title=xyz></a>";
 	var e = $(htmlCode).appendTo("#qunit-fixture").editable();
 	e.click();
 	var p = e.data("editableContainer").tip();
@@ -36,9 +40,38 @@ QUnit.test("LT Config Editor Test", function(assert) {
 	simpleTest("rampdown", "Ramp Down displays");
 	simpleTest("target", "Target displays");
 	simpleTest("port", "Port displays");
+	var $load = p.find("div#load");
+	var $select = $load.find("a");
+	assert.equal($select.text(), "Select To Add New Scheme",
+				 "new user load: the initial value of rps is correct");
+	assert.equal($select.attr("data-rps"), testData.rps,
+				 "new user load: value view");
 	p.find(".editable-cancel").click();
 	setTimeout(function() {
 		assert.ok(!p.is(":visible"), "popover was removed");
 		done();
 	}, 300);
+});
+
+QUnit.test("LT Config Editor: the 'Plus' Button", function(assert) {
+	var done = assert.async();
+	var binTestData = jsonstr2bin(JSON.stringify(testData));
+	var htmlCode = "<a href=# id=a data-type=ltconfigeditor " +
+				   "data-value='" + binTestData + "' data-title=xyz></a>";
+	var e = $(htmlCode).appendTo("#qunit-fixture").editable();
+	e.click();
+	var p = e.data("editableContainer").tip();
+	var $load = p.find("div#load");
+	var $button = $load.find("button");
+	assert.ok($button, "the 'Plus' button shows");
+	assert.ok($button.attr("disabled"), "the 'Plus' button must be disabled");
+	var $select = $load.find("a");
+	$select.click();
+
+	var $selectEditable = $select.data("editableContainer").tip();
+	$selectEditable.find("select").val("const");
+	$selectEditable.find(".editable-submit");
+
+	$button = $load.find("button");
+	assert.ok(!$button.attr("disabled"), "the 'Plus' button must be enabled");
 });
--- salts/static/salts/js/ltconfigeditor.js
+++ salts/static/salts/js/ltconfigeditor.js
@@ -1,7 +1,24 @@
 (function ($) {
 	"use strict";
 
+	var addLoad = function(option, rps) {
+		var $tbl = $("div#load table");
+		var labelCode = "<label><span>Load: </span></label>";
+		var aCode = $.htmlCodeLTSelectSchedule(option, rps);
+		var butCode = "<button type=button class='btn btn-plus' disabled>" +
+						"<span class='glyphicon glyphicon-plus'></span>" +
+					  "</button>";
+		$tbl.append("<tr class=salts-load-row><td>" + labelCode + "</td><td>" +
+					aCode + "</td><td>" + butCode + "</td></tr>");
+		return $tbl.find("tr:last");
+	}
+
 	var LTConfigEditor = function (options) {
+		console.log("Init LTConfigEditor. Options: " + JSON.stringify(options));
+		var value = valueFromObject(options.scope, "value");
+		var data = JSON.parse(bin2jsonstr(value));
+		this.rps = data.rps;
+		console.log("Init LTConfigEditor. RPS: " + this.rps);
 		this.init("ltconfigeditor", options, LTConfigEditor.defaults);
 	};
 
@@ -11,6 +28,19 @@
 
 		render: function() {
 			this.$input = this.$tpl.find("input");
+			var $row = addLoad("no", this.rps);
+			this.$load = $row.find("a");
+			var $load = this.$load;
+			this.$load.editable({
+				display: function(value) {
+					console.log("Load. value: " + value);
+					$load.text(value);
+				}
+			});
+			this.$load.on("save", function(e, params) {
+				var disabled = (params.newValue == $.LTSelectOptions.no);
+				$row.find("button").attr("disabled", disabled);
+			});
 		},
         
 		value2html: function(value, element) {
@@ -36,7 +66,7 @@
 			this.$input.filter("[name='test_name']").val(changed["test_name"]);
 			this.$input.filter("[name='rps']").val(changed["rps"]);
 			this.$input.filter("[name='rampup']").val(ms2sec(changed["rampup"]));
-			this.$input.filter("[name='testlen']").val(ms2sec(changed["testlen"]));
+			/*this.$input.filter("[name='testlen']").val(ms2sec(changed["testlen"]));*/
 			this.$input.filter("[name='rampdown']").val(ms2sec(changed["rampdown"]));
 			this.$input.filter("[name='target']").val(changed["target"]);
 			this.$input.filter("[name='port']").val(changed["port"]);
@@ -48,7 +78,7 @@
 				test_name: this.$input.filter("[name='test_name']").val(),
 				rps: this.$input.filter("[name='rps']").val(),
 				rampup: sec2ms(this.$input.filter("[name='rampup']").val()),
-				testlen: sec2ms(this.$input.filter("[name='testlen']").val()),
+				/*testlen: sec2ms(this.$input.filter("[name='testlen']").val()),*/
 				rampdown: sec2ms(this.$input.filter("[name='rampdown']").val()),
 				target: this.$input.filter("[name='target']").val(),
 				port: this.$input.filter("[name='port']").val(),
@@ -64,12 +94,22 @@
 
 	LTConfigEditor.defaults = $.extend({},
 		$.fn.editabletypes.abstractinput.defaults, {
-			tpl: "<div><label><span>Имя теста: </span></label><input type='text' name='test_name'></input></div>" +
-				 "<div><label><span>RPS: </span></label><input type='text' name='rps'></input></div>" +
-				 "<div><label><span>Ramp Up: </span></label><input type='text' name='rampup'></input></div>" +
-				 "<div><label><span>Ramp Down: </span></label><input type='text' name='rampdown'></input></div>" +
-				 "<div><label><span>Target: </span></label><input type='text' name='target'></input>" +
-				 "<label><span>Port: </span></label><input type='text' name='port'></input></div>",
+			tpl: "<div><label><span>Имя теста: </span></label>" +
+					"<input type='text' name='test_name'></input></div>" +
+				 "<div><label><span>RPS: </span></label>" +
+					"<input type='text' name='rps'></input></div>" +
+				 "<div><label><span>Ramp Up: </span></label>" +
+			     "<input type='text' name='rampup'></input></div>" +
+				 "<div id=load>" +
+					"<table>" +
+					"</table>" +
+				 "</div>" +
+				 "<div><label><span>Ramp Down: </span></label>" +
+					"<input type='text' name='rampdown'></input></div>" +
+				 "<div><label><span>Target: </span></label>" +
+					"<input type='text' name='target'></input>" +
+				 	"<label><span>Port: </span></label>" +
+					"<input type='text' name='port'></input></div>",
 			inputclass: ""
 	});
 
