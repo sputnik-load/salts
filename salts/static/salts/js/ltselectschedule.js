7a7fc1dcdd879954996f815cfb97e8e7a6cb9479
Ilya Krylov <krylov@corp.sputnik.ru>
2017-02-13 10:47:45 +0300

    Корректно отображаются поля редактирования параметров при выборе профиля нагрузки.

--- salts/static/salts/js/ltselectschedule.js
+++ salts/static/salts/js/ltselectschedule.js
@@ -21,11 +21,22 @@
 				"data-source='" + srcValuesLine + "'></a>";
 	}
 
+
+	var loadScheduleParam = function(th, $ss) {
+		$ss.find("#param").detach();
+		var value = $ss.find("#schedule").val();
+		if (value == "const")
+			th.$constParam.appendTo("#select-schedule");
+		else if (value == "step")
+			th.$stepParam.appendTo("#select-schedule");
+		else
+			$("<div id=param></div>").appendTo("#select-schedule");
+	}
+
 	var LTSelectSchedule = function (options) {
 		this.init("ltselectschedule", options, LTSelectSchedule.defaults);
 		this.rps = valueFromObject(this.options.scope, "rps");
 		this.source = valueFromObject(this.options.scope, "source");
-		this.value = valueFromObject(this.options.scope, "value");
 		this.$constParam = $("<div id=param>" +
 							 "<label><span>Duration: </span></label>" +
 							 	"<input type=text name=duration></input>" +
@@ -66,6 +77,11 @@
 			if (!value)
 				return;
 			$("#schedule option").remove();
+			var th = this;
+			var $schedule = $("#schedule");
+			$schedule.on("change", function() {
+				loadScheduleParam(th, $("#select-schedule"));
+			});
 			for (var i = 0; i < this.source.length; i++) {
 				var item = this.source[i];
 				if (item[0] == "no" && item[1] != value)
@@ -73,25 +89,11 @@
 				$("#schedule").append($("<option></option>")
 										.attr("value", item[0])
 										.text(item[1]));
-				if (item[1] == value)
-					$("#schedule").val(item[0]);
+				if (item[1] == value) {
+					$schedule.val(item[0]);
+					loadScheduleParam(th, $("#select-schedule"));
+				}
 			}
-			var $constParam = this.$constParam;
-			var $stepParam = this.$stepParam;
-			$("#schedule").on("change", function() {
-				if (this.value == "const") {
-					$("#select-schedule").find("#param").detach();
-					$constParam.appendTo("#select-schedule");	
-				}
-				else if (this.value == "step") {
-					$("#select-schedule").find("#param").detach();
-					$stepParam.appendTo("#select-schedule");	
-				}
-				else {
-					$("#select-schedule").find("#param").detach();
-					$("<div id=param></div>").appendTo("#select-schedule");
-				}
-			});
 		},
        
 		input2value: function() {
